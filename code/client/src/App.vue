062d4b39ea92d45b541fe9f844b9a632c7d75b67
Arnur Kushenov <bocunahero@gmail.com>
2025-11-27 15:00:04 +0500

    client updated

--- code/client/src/App.vue
+++ code/client/src/App.vue
@@ -1,17 +1,12 @@
-<template lang="pug">
-   #app
-    b-navbar(toggleable="lg" type="dark" variant="info")
-      b-navbar-brand 
-        | Proxy Management 
-        b-badge v2.0
-      b-navbar-nav
-        b-nav-item(to="/") Proxy
-        b-nav-item(to="/ip_logs") IP History
-        b-nav-item(to="/speed_logs") Speed History
-        b-nav-item(to="/visit_logs") Visit History
-    router-view
+<template>
+  <ThemeProvider>
+    <SidebarProvider>
+      <RouterView />
+    </SidebarProvider>
+  </ThemeProvider>
 </template>
 
-<style>
-
-</style>
+<script setup lang="ts">
+import ThemeProvider from './components/layout/ThemeProvider.vue'
+import SidebarProvider from './components/layout/SidebarProvider.vue'
+</script>
